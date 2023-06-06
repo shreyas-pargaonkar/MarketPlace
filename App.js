327316eca888e2db4b54e03ef365019b23ff322a
shreyas-pargaonkar <shreyaspargaonkar@gmail.com>
2023-06-07 04:53:49 +0530

    shopping cart

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ProductScreen from "./src/data/Screens/ProductScreen";
 import ProductDetailsScreen from "./src/data/Screens/ProductDetailsScreen";
+import ShoppingCart from "./src/data/Screens/ShoppingCart";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <ProductScreen />
-       */}
-      <ProductDetailsScreen />
+      {/* <ProductScreen />*/}
+      {/* <ProductDetailsScreen /> */}
+      <ShoppingCart />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/data/Screens/ShoppingCart.js
@@ -0,0 +1,80 @@
+import { FlatList, Text, View, StyleSheet, Pressable } from "react-native";
+import cart from "../cart";
+import Cart from "../cart";
+import CartListItem from "../Components/CartListItem";
+
+const ShoppingCart = () => {
+  const addToCart = () => {
+    console.warn("Added to cart");
+  };
+  return (
+    <>
+      <FlatList
+        data={cart}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        ListFooterComponent={() => (
+          <View style={styles.totalContainer}>
+            <View style={styles.row}>
+              <Text style={styles.text}>SubTotal</Text>
+              <Text style={styles.text}>$320.0</Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.text}>Delivery</Text>
+              <Text style={styles.text}>$320.0</Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.total}>Total</Text>
+              <Text style={styles.price}>$320.0</Text>
+            </View>
+          </View>
+        )}
+      />
+      <Pressable onPress={addToCart} style={styles.button}>
+        <Text style={styles.buttonText}>Checkout</Text>
+      </Pressable>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  totalContainer: {
+    margin: 20,
+    paddingTop: 10,
+    borderColor: "gainsboro",
+    borderTopWidth: 1,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 2,
+  },
+  text: {
+    fontSize: 16,
+    color: "gray",
+  },
+  total: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  button: {
+    position: "absolute",
+    bottom: 30,
+    backgroundColor: "black",
+    width: "90%",
+    alignSelf: "center",
+    padding: 20,
+    alignItems: "center",
+    borderRadius: 100,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
+
+export default ShoppingCart;
